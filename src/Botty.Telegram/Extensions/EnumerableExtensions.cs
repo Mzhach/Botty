85f121a1790e4cfa14fde6d9465be9c229640687
Илья Мжачев <i.mzhachev@yandex.ru>
2022-10-21 21:13:43 +0300

    Add EnumerableExtensions

---
+++ src/Botty.Telegram/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Botty.Telegram.Extensions
+{
+    /// <summary>
+    /// Extensions for IEnumerable
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Checks enumerable to be empty
+        /// </summary>
+        public static bool IsEmpty<T>(this IEnumerable<T>? value)
+            => value == null || !value.Any();
+
+        /// <summary>
+        /// Checks enumerable not to be empty
+        /// </summary>
+        public static bool IsNotEmpty<T>(this IEnumerable<T>? value)
+            => value != null && value.Any();
+    }
+}
---
+++ tests/Botty.Telegram.Tests/Extensions/EnumerableExtensionsTests.cs
@@ -0,0 +1,67 @@
+﻿using Botty.Telegram.Extensions;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Botty.Telegram.Tests.Extensions
+{
+    public class EnumerableExtensionsTests
+    {
+        public static IEnumerable<object[]> EmptyCases 
+            => new List<object[]>
+            {
+                new object[] { null },
+                new object[] { new List<int>() }
+            };
+        public static IEnumerable<object[]> NotEmptyCases
+            => new List<object[]>
+            {
+                new object[] { new List<int> { 1, 2 ,3 } }
+            };
+
+        [Theory]
+        [MemberData(nameof(EmptyCases))]
+        public void IsEmpty_ShouldReturnTrue(IEnumerable<int> enumerable)
+        {
+            // Act
+            var actual = enumerable.IsEmpty();
+
+            // Assert
+            actual.Should().BeTrue();
+        }
+
+        [Theory]
+        [MemberData(nameof(NotEmptyCases))]
+        public void IsEmpty_ShouldReturnFalse(IEnumerable<int> enumerable)
+        {
+            // Act
+            var actual = enumerable.IsEmpty();
+
+            // Assert
+            actual.Should().BeFalse();
+        }
+
+        [Theory]
+        [MemberData(nameof(NotEmptyCases))]
+        public void IsNotEmpty_ShouldReturnTrue(IEnumerable<int> enumerable)
+        {
+            // Act
+            var actual = enumerable.IsNotEmpty();
+
+            // Assert
+            actual.Should().BeTrue();
+        }
+
+        [Theory]
+        [MemberData(nameof(EmptyCases))]
+        public void IsNotEmpty_ShouldReturnFalse(IEnumerable<int> enumerable)
+        {
+            // Act
+            var actual = enumerable.IsNotEmpty();
+
+            // Assert
+            actual.Should().BeFalse();
+        }
+    }
+}
