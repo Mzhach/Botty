f204fbee82f3d9af9f057387877066fd3db84766
Илья Мжачев <i.mzhachev@yandex.ru>
2022-11-01 21:14:13 +0300

    Add test for serialization DateTime to Unix timestamp

--- tests/Botty.Telegram.Tests/Converters/Json/UnixDateTimeConverterTests.cs
+++ tests/Botty.Telegram.Tests/Converters/Json/UnixDateTimeConverterTests.cs
@@ -1,4 +1,5 @@
-﻿using Botty.Telegram.Converters.Json;
+﻿using AutoFixture;
+using Botty.Telegram.Converters.Json;
 using FluentAssertions;
 using System;
 using System.Text.Json;
@@ -8,6 +9,7 @@ namespace Botty.Telegram.Tests.Converters.Json
 {
     public class UnixDateTimeConverterTests
     {
+        private readonly Fixture _fixture = new();
         private readonly JsonSerializerOptions _options = new()
         {
             Converters = { new UnixDateTimeConverter() }
@@ -30,6 +32,26 @@ namespace Botty.Telegram.Tests.Converters.Json
             // Assert
             testObject.Should().NotBeNull();
             testObject!.DateTime.Should().Be(new DateTime(2022, 10, 18, 19, 00, 34, DateTimeKind.Utc));
+        }
+
+        [Fact]
+        public void Serializer_ShouldSerializeDateTimeToUnixTimestamp()
+        {
+            // Arrange
+            var testObject = _fixture
+                .Build<TestClass>()
+                .Create();
+
+            var expected = ((DateTimeOffset)testObject.DateTime).ToUnixTimeSeconds();
+
+            // Act
+            var serializedTestObject = JsonSerializer.Serialize(testObject, _options);
+
+            // Assert
+            serializedTestObject.Should().NotBeNullOrEmpty();
+            var jsonDocument = JsonDocument.Parse(serializedTestObject);
+            var dateTimeProperty = jsonDocument.RootElement.GetProperty(nameof(TestClass.DateTime));
+            dateTimeProperty.GetInt64().Should().Be(expected);
         }
     }
 }
